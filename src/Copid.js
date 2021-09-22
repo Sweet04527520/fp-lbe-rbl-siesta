9651d307221ba8c4812401e71bdc9fdac2f1303d
Okyan Awang Ramadhana <okyanawang@gmail.com>
2021-09-22 23:16:58 +0700

    Adding todos

--- src/Copid.js
+++ src/Copid.js
@@ -2,9 +2,26 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function Copid() {
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const [items, setItems] = useState([]);
+
+  const fetchItems = async () => {
+    const data = await fetch(
+      'http://fortnite-public-api.theapinetwork.com/prod09/upcoming/get'
+    );
+
+    const items = await data.json();
+    console.log(items);
+    setItems(items);
+  }
   return (
     <div>
-      <h1>Nani Copid?</h1>
+      {items.map(item => (
+        <h1>{items.name}</h1>
+      ))}
     </div>
   );
 }
--- src/Todopage.js
+++ src/Todopage.js
@@ -1,11 +1,69 @@
-import React from 'react';
-import './App.css';
+import React, { useState,useEffect } from 'react';
+import './components/Todostyle.css';
+import Form from "./components/Form"
+import ToDoList from './components/ToDoList';
 
 function Todopage() {
+  //State stuff
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  
+  // //Run once when npm start
+  // useEffect(() => {
+  //   getLocalTodos();
+  // }, []);
+  //Use effect
+  useEffect(() => {
+    filterHandler();
+    // saveLocalTodos();
+  }, [todos, status]);
+  //Functions
+  const filterHandler = () => {
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  }
+  //save to local
+  // const saveLocalTodos = () => {
+  //   localStorage.setItem("todos", JSON.stringify(todos));
+  // };
+  // const getLocalTodos = () = {
+  //   if(localStorage.getItem("todos") === null) { 
+  //     localStorage.setItem("todos", JSON.stringify([]));
+  //   } else {
+  //     let todoLocal = JSON.parse(localStorage.getItem("todos"))
+  //     setTodos(todoLocal);
+  //   }
+  // };
+
   return (
-    <div>
-      <h1>Todo</h1>
-    </div>
+    <div className="App">
+      <header>
+        <h1>Siesta's to do list</h1>
+      </header>
+      <Form 
+        inputText={inputText}
+        setTodos={setTodos}
+        setStatus={setStatus}
+        todos={todos} 
+        setInputText={setInputText}
+      />
+      <ToDoList 
+        filteredTodos={filteredTodos}
+        setTodos={setTodos} 
+        todos={todos}
+      />
+    </div> 
   );
 }
 
