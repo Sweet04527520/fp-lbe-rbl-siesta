e8d0d61b8c7fd9bc4d751525627d8ba4b243f951
Okyan Awang Ramadhana <okyanawang@gmail.com>
2021-09-22 21:13:09 +0700

    Sie's first commit

---
+++ src/Copid.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function Copid() {
+  return (
+    <div>
+      <h1>Nani Copid?</h1>
+    </div>
+  );
+}
+
+export default Copid;
---
+++ src/Todopage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Todopage() {
+  return (
+    <div>
+      <h1>Todo</h1>
+    </div>
+  );
+}
+
+export default Todopage;
